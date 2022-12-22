b32c9b508a60023d4d69de31d47a955f9a77f863
Souza <kleytonl.souza@gmail.com>
2022-12-22 12:12:24 -0300

    add

--- base.py
+++ base.py
@@ -1,7 +1,9 @@
 import os
+
 import click
 from flask_migrate import Migrate
-from app import create_app, db
+
+from app import create_app
 from app.models import *
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flask]'
+    FLASKY_MAIL_SENDER = 'Flask Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASK_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
