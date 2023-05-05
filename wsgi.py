f2f1374fae6b4b2383872486b87440242e56df40
kleytonsouza <44651763+kleytonsouza@users.noreply.github.com>
2023-05-05 10:47:58 -0300

    Update wsgi.py
    add code to fix 'missing Vary: Cookie header'

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
 from base import app
 
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
+
+response.set_cookie('username', 'flask', secure=True, httponly=True, samesite='Lax')
+
 if __name__ == "__main__":
     app.run()
