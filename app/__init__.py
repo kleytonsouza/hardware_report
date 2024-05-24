969aa913c575201759efeb01c55496f9a2bf5801
kleytonsouza <kleytonl.souza@gmail.com>
2024-05-23 22:32:51 -0300

    first commit positivo

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
 
-def create_app(config_name):
+def create_app(config_name="default"):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
